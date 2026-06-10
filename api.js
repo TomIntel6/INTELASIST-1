6dccf57e0fa4f16b586ba0f7e171efc9a1a42807
JoseRodriguez <baex10@icloud.com>
2026-06-09 22:23:08 -0500

    fix: express 5 cors compatibility

--- api.js
+++ api.js
@@ -94,7 +94,6 @@ const corsOptions = {
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
 
 const authRoutes = express.Router()
 
