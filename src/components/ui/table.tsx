9d854867b8854667dae9d956b41b70d8639133e4
Jose Rodriguez <baex10@icloud.com>
2026-08-06 05:56:20 -0500

    UI: soften table row hover globally and ensure text remains legible

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -57,7 +57,7 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
     <tr
       data-slot="table-row"
       className={cn(
-        "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+        "border-b transition-colors group hover:bg-muted/8 data-[state=selected]:bg-muted",
         className
       )}
       {...props}
@@ -83,7 +83,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
     <td
       data-slot="table-cell"
       className={cn(
-        "p-2 align-middle break-words whitespace-normal [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+        "p-2 align-middle break-words whitespace-normal [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] group-hover:text-foreground",
         className
       )}
       {...props}
